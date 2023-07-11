9fa7ac850429171edbfb91aa778870dbeb968b42
Jean-Baptiste-Paris <ecrireajb@gmail.com>
2023-07-11 09:46:48 +0200

    Update pieces.js && index.html Intégration de listes de pièces filtrées
    Signed-off-by: Jean-Baptiste-Paris <ecrireajb@gmail.com>

--- pieces.js
+++ pieces.js
@@ -4,10 +4,12 @@ const pieces = await reponse.json();
 
 for (let i = 0; i < pieces.length; i++) {
 
-    const sectionFiches = document.querySelector(".fiches");
-    // Création des balises 
-    const pieceElement = document.createElement("article");
     const article = pieces[i];
+    // Récupération de l'élément du DOM qui accueillera les fiches
+    const sectionFiches = document.querySelector(".fiches");
+    // Création d’une balise dédiée à une pièce automobile
+    const pieceElement = document.createElement("article");
+    // Création des balises 
     const imageElement = document.createElement("img");
     imageElement.src = article.image;
     const nomElement = document.createElement("h2");
@@ -16,38 +18,31 @@ for (let i = 0; i < pieces.length; i++) {
     prixElement.innerText = `Prix: ${article.prix} € (${article.prix < 35 ? "€" : "€€€"})`;
     const categorieElement = document.createElement("p");
     categorieElement.innerText = article.categorie ?? "(aucune catégorie)";
-    const descritpionElement = document.createElement("p");
-    descritpionElement.innerText = article.description ?? "Pas de description pour le moment.";
-    const disponibiliteElement = document.createElement("p");
-    disponibiliteElement.innerText = article.disponibilite ? "En stock" : "Rupture de stock";
+    const descriptionElement = document.createElement("p");
+    descriptionElement.innerText = article.description ?? "Pas de description pour le moment.";
+    const stockElement = document.createElement("p");
+    stockElement.innerText = article.disponibilite ? "En stock" : "Rupture de stock";
     
-    //Rattachement de nos balises au DOM
+    // On rattache la balise article a la section Fiches
     sectionFiches.appendChild(pieceElement);
+    // On rattache l’image à pieceElement (la balise article)
     pieceElement.appendChild(imageElement);
     pieceElement.appendChild(nomElement);
     pieceElement.appendChild(prixElement);
     pieceElement.appendChild(categorieElement);
-    pieceElement.appendChild(descritpionElement);
-    pieceElement.appendChild(disponibiliteElement);
-}
+    //Ajout des éléments au DOM pour l'exercice
+    pieceElement.appendChild(descriptionElement);
+    pieceElement.appendChild(stockElement);
 
-//gestion des bouttons 
+ }
+ 
+ //gestion des boutons 
 const boutonTrier = document.querySelector(".btn-trier");
 
 boutonTrier.addEventListener("click", function () {
     const piecesOrdonnees = Array.from(pieces);
     piecesOrdonnees.sort(function (a, b) {
         return a.prix - b.prix;
-    });
-    console.log(piecesOrdonnees);
-});
-
-const boutonDecroissant = document.querySelector(".btn-decroissant");
-
-boutonDecroissant.addEventListener("click", function () {
-    const piecesOrdonnees = Array.from(pieces);
-    piecesOrdonnees.sort(function (a, b) {
-        return b.prix - a.prix;
      });
      console.log(piecesOrdonnees);
 });
@@ -61,11 +56,52 @@ boutonFiltrer.addEventListener("click", function () {
    console.log(piecesFiltrees)
 });
 
-const boutonNoDescription = document.querySelector(".btn-nodesc");
+const boutonDecroissant = document.querySelector(".btn-decroissant");
+boutonDecroissant.addEventListener("click", function () {
+    const piecesOrdonnees = Array.from(pieces);
+    piecesOrdonnees.sort(function (a, b) {
+        return b.prix - a.prix;
+     });
+     console.log(piecesOrdonnees);
+});
 
+const boutonNoDescription = document.querySelector(".btn-nodesc");
 boutonNoDescription.addEventListener("click", function () {
     const piecesFiltrees = pieces.filter(function (piece) {
-        return piece.description
+        return piece.description;
     });
-   console.log(piecesFiltrees)
+   console.log(piecesFiltrees);
 });
+
+const noms = pieces.map(piece => piece.nom);
+for(let i = pieces.length -1 ; i >= 0; i--){
+    if(pieces[i].prix > 35){
+        noms.splice(i,1);
+    }
+}
+
+//Création de la liste
+const abordablesElements = document.createElement('ul');
+//Ajout de chaque nom à la liste
+for(let i=0; i < noms.length ; i++){
+    const nomElement = document.createElement('li');
+    nomElement.innerText = noms[i];
+    abordablesElements.appendChild(nomElement)
+}
+// Ajout de l'en-tête puis de la liste au bloc résultats filtres
+document.querySelector('.abordables')
+    .appendChild(abordablesElements);
+
+//Création de la liste
+const disponiblesElements = document.createElement('ul');
+for(let i=0; i < pieces.length; i++){
+    // Recherche des pièces disponibles
+    if(pieces[i].disponibilite){
+        const nomPrixElement = document.createElement('li');
+        nomPrixElement.innerText = `${pieces[i].nom} - ${pieces[i].prix} €`;
+        disponiblesElements.appendChild(nomPrixElement)
+    }
+}
+// Ajout de la liste au bloc .disponibles
+document.querySelector('.disponibles')
+    .appendChild(disponiblesElements)
